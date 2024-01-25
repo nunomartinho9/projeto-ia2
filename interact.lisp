b55e7a16ae641bf7b31c92ba2fdb4590dba7f8ae
João Coelho <84146833+joaortcoelho@users.noreply.github.com>
2024-01-25 15:34:10 +0000

    compile&load code and .gitignore updated

--- interact.lisp
+++ interact.lisp
@@ -1,1 +1,11 @@
 ;; funções que permitem escrever e ler em ficheiros e tratar da interação com o utilizador 
+
+(defun diretorio ()
+    "C:/Users/joaor/OneDrive/Documentos/IPS/ESTS/LEI/3_ANO/IA/Projeto/ia-projeto2/"
+;;    "U:/Documents/LEI/AnoCorrente/ia/PROJETO/"
+)
+
+(compile-file (concatenate 'string (diretorio) "algoritmo.lisp"))
+(compile-file (concatenate 'string (diretorio) "jogo.lisp"))
+(load (concatenate 'string (diretorio) "algoritmo.lisp"))
+(load (concatenate 'string (diretorio) "jogo.lisp"))
